0198df38b030b130dcee9c4dab3a9c870d458657
Arthur Pierrot <arthur.pierrot@hotmail.com>
2022-10-03 22:19:44 +0200

    Initial commit

---
+++ src/main/java/rpmod/rpmod/RPMod.java
@@ -0,0 +1,10 @@
+package rpmod.rpmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RPMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/rpmod/rpmod/client/RPModClient.java
@@ -0,0 +1,13 @@
+package rpmod.rpmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class RPModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
